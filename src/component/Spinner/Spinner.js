e8a48f50230adfadf152a864f215981c04439a2a
Nicolas Mahler <nicolasmahler@me.com>
2019-06-09 01:09:49 -0400

    red spinner

--- src/component/Spinner/Spinner.js
+++ src/component/Spinner/Spinner.js
@@ -3,10 +3,10 @@ import './Spinner.css';
 
 const Spinner = (props) => {
     return (
-        <div class="wrapper" style={{...props.transitionStyles[props.state]}}>
-            <div class="ball ball-1"></div>
-            <div class="ball ball-2"></div>
-            <div class="ball ball-3"></div>
+        <div className="wrapper" style={{...props.transitionStyles[props.state]}}>
+            <div className="ball ball-1"></div>
+            <div className="ball ball-2"></div>
+            <div className="ball ball-3"></div>
         </div>
     )
 }
