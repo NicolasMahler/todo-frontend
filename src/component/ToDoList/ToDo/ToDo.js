e0bfa3bd0e9164a24566c5d8ed1adf16067121b5
Nicolas Mahler <nicolasmahler@me.com>
2019-06-09 01:10:06 -0400

    new to do component

---
+++ src/component/ToDoList/ToDo/ToDo.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const ToDo = ({todo}) => {
+
+    useEffect(() => {
+        console.log(todo);
+    })
+
+    return (
+        <
+    )
+
+}
+
+export default ToDo;
