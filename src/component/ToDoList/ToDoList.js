27875b93afa628c8f4653bf7f592bb591f5c9ee5
Nicolas Mahler <nicolasmahler@me.com>
2019-06-10 03:11:03 -0400

    call get todo once

--- src/component/ToDoList/ToDoList.js
+++ src/component/ToDoList/ToDoList.js
@@ -1,19 +1,17 @@
 import React, {useState, useEffect} from 'react';
-import ToDo from './ToDo/ToDo';
+import ToDoContainer from './ToDo/ToDoContainer';
 import { connect } from 'react-redux';
 import ToDoStore from '../../store/ToDos/ToDoStore';
 
 const ToDoList = ({getTodos, todos}) => {
     
     useEffect(() => {
-        if (todos.length === 0) {
-            getTodos(localStorage.getItem('token'));
-        }
-    });
+        getTodos(localStorage.getItem('token'));
+    }, []);
 
     return (
         <React.Fragment>
-            {todos.forEach(todo => (<ToDo todo={todo} />))};
+            {todos.map(todo => (<ToDoContainer key={todo.id} todo={todo} />))}
         </React.Fragment>
     );
 
