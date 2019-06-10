90acada260495da8ed95485c61f4e87e279afa3b
Nicolas Mahler <nicolasmahler@me.com>
2019-06-10 03:10:43 -0400

    for to htmlFor and class to className

--- src/component/addMenu/AddMenu.js
+++ src/component/addMenu/AddMenu.js
@@ -7,13 +7,13 @@ export default function AddMenu() {
             <div className="menu-container">
                 <nav className="menu">
                     <input type="checkbox" href="#" className="menu-open" name="menu-open" id="menu-open"/>
-                    <label className="menu-open-button" for="menu-open">
+                    <label className="menu-open-button" htmlFor="menu-open">
                         <span className="hamburger hamburger-1"></span>
                         <span className="hamburger hamburger-2"></span>
                         <span className="hamburger hamburger-3"></span>
                     </label>
                     <button type="button" className="menu-item"> <i className="fa fa-id-card"></i> </button>
-                    <button dsabled={true} type="button" className="menu-item"> <i className="fa fa-cogs"></i> </button>
+                    <button disabled={true} type="button" className="menu-item"> <i className="fa fa-cogs"></i> </button>
                     <button type="button" className="menu-item"> <i className="fa fa-plus"></i> </button>
                 </nav>
             </div>
