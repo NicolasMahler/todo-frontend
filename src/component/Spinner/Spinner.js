15fa1d2a211fd4592239eb1d4dac9c56ff02eee2
Nicolas Mahler <nicolasmahler@me.com>
2019-07-26 11:39:52 -0400

    add blur to spinner so it isn't always in dom

--- src/component/Spinner/Spinner.js
+++ src/component/Spinner/Spinner.js
@@ -7,6 +7,15 @@ const Spinner = (props) => {
             <div className="ball ball-1"></div>
             <div className="ball ball-2"></div>
             <div className="ball ball-3"></div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                <defs>
+                <filter id="goo">
+                    <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10" />
+                    <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+                    <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+                </filter>
+                </defs>
+            </svg>
         </div>
     )
 }
