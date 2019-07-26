c7a76ba5ffe4a507ffa38df2c0cd603cb7fedcf8
Nicolas Mahler <nicolasmahler@me.com>
2019-07-26 13:15:39 -0400

    add exiting and exited styles for clarity to transitionStyles

--- src/views/CreateAccount/CreateAccount.js
+++ src/views/CreateAccount/CreateAccount.js
@@ -9,6 +9,8 @@ import UserStore from '../../store/User/UserStore';
 const transitionStyles = {
     entering: { opacity: '0' },
     entered: { opacity: '1' },
+    exiting: { opacity: '0' },
+    exited: { opacity: '0' },
 }
 
 class CreateAccount extends Component {
