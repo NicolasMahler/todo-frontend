02f88c11dcee881b7517e81d4df716819a1d571d
Nicolas Mahler <nicolasmahler@me.com>
2019-07-26 13:12:15 -0400

    remove unused useState import

--- src/component/ToDoList/ToDoList.js
+++ src/component/ToDoList/ToDoList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import ToDoContainer from './ToDo/ToDoContainer';
 import { connect } from 'react-redux';
 import ToDoStore from '../../store/ToDos/ToDoStore';
