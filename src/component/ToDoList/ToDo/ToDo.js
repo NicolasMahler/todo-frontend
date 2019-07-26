35bad9682cf91be88c93e44bb014668296ecc38f
Nicolas Mahler <nicolasmahler@me.com>
2019-07-26 13:12:36 -0400

    remove old html

--- src/component/ToDoList/ToDo/ToDo.js
+++ src/component/ToDoList/ToDo/ToDo.js
@@ -25,18 +25,7 @@ function ToDo({todo, onChange, onDelete, style, onCompleted}) {
 
     return (
         <form className="todo__container" style={style}>
-            <input type="checkbox" name="completed" id="completed" onChange={completedOnChange} value={completed} hidden />
-            {completed ?
-                <label htmlFor="completed" className="todo__label"><i className="fa fa-check"></i></label> :
-                <label htmlFor="completed" className="todo__label"><i className="fa fa-times"></i></label>
-            }
-            <input className="todo__textInput" type="text" name="todo" id="todo" onChange={textOnChange} value={text} disabled={!edit} />
-            <input type="checkbox" name="edit" id="edit" value={edit} onChange={editOnChange} hidden />
-            {edit ? 
-                <label htmlFor="edit" className="todo__label"><i className="fa fa-save"></i></label> : 
-                <label htmlFor="edit" className="todo__label"><i className="fa fa-edit"></i></label>
-            }
-            <button className="todo__delete" type="button" onClick={onDelete}><i className="fa fa-trash"></i></button>
+            <input contentEditable className="todo__textInput" type="text" name="todo" id={"todo" + todo.id} onChange={textOnChange} value={text} disabled={!edit}/>
         </form>
     )
 
