5ef638e9ca6c5ba96009e4ed16e9874fcc6f1770
Nicolas Mahler <nicolasmahler@me.com>
2019-07-26 13:10:59 -0400

    clean up function file

--- src/store/ToDos/function.js
+++ src/store/ToDos/function.js
@@ -24,8 +24,7 @@ export function createErrorLoading(errorMessage) {
 }
 export function createToDo(todo) {
     return { type: actions.CREATE_TODO, todo }
-} 
-
+}
 export function deleteToDo(id) {
     return { type: actions.DELETE_TODO, id }
 }
